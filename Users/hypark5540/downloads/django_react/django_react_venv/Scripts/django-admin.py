0a10124533bb04e3463ad5f155c8e1b51a9e0f2f
unknown <hypark5540@bluehole.net>
2019-09-10 09:28:17 +0900

    clean push / hypark5540

---
+++ Users/hypark5540/downloads/django_react/django_react_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hypark5540\downloads\django_react\django_react_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
